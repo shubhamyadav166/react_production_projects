21e4df3ca485b2ce8800440cfc9c09efb8d55415
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-03-25 14:30:21 +0530

    redux toolkit todo app finaly complete and debug

--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RiDeleteBinLine } from "react-icons/ri";
 import { useSelector, useDispatch } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlices'
 function Todos() {
@@ -9,17 +10,22 @@ function Todos() {
     const dispatch = useDispatch()
     return (
         <>
-            <div>Todos</div>
-            {todos.map((todo) => (
+            <div className='flex justify-center item-center flex-col w-full'>
+                <h1 className='text-center text-xl mt-10 text-6xl font-bold'>Todos</h1>
+                {todos.map((todo) => (
 
-                <li key={todo.key}>
-                    {todo.text}
-                    <button
-                        className='p-2 bg-red-500 text-white'
-                        onClick={() => dispatch(removeTodo(todo.id))}
-                    >X</button>
-                </li>
-            ))}
+                    <div className='w-full flex justify-center'> <li key={todo.key}
+                        className='w-3/4 lg:w-1/2 relative bg-black rounded mt-5 text-white text-3xl lg:px-5'
+                    >
+                        {todo.text}
+                        <button
+                            className='bg-red-500 text-white absolute right-0 rounded h-full px-2'
+                            onClick={() => dispatch(removeTodo(todo.id))}
+                        ><RiDeleteBinLine /></button>
+                    </li>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
--- reduxToolkitTodo/src/components/addTodo.jsx
+++ reduxToolkitTodo/src/components/addTodo.jsx
@@ -5,7 +5,7 @@ import { addTodo } from '../features/todo/todoSlices'
 function AddTodo() {
 
     const [input, setInput] = useState('')
-    console.log(input);
+    console.log(input); 0
 
     const dispatch = useDispatch()
     const addTodoHandler = (e) => {
@@ -17,19 +17,21 @@ function AddTodo() {
 
     return (
         <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
-            <input
-                type="text"
-                className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                placeholder="Enter a Todo..."
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <button
-                type="submit"
-                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            >
-                Add Todo
-            </button>
+            <div className='w-full flex flex-row gap-10 justify-center'>
+                <input
+                    type="text"
+                    className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    placeholder="Enter a Todo..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button
+                    type="submit"
+                    className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                >
+                    Add Todo
+                </button>
+            </div>
         </form>
     )
 }
